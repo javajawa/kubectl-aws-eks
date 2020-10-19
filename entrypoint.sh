268932d488523d0def786e48c3b411d411d4ae6c
Ben Harcourt <ben.harcourt@myvouchercodes.co.uk>
2020-10-19 14:00:16 +0000

    temp

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 set -e
-set -x
 
 export readonly KUBECONFIG=/tmp/config
 
 if [ -z "$KUBE_CONFIG_DATA" ]
 then
-	aws eks update-kubeconfig --name "$KUBE_CLUSTER" --kubeconfig "$KUBECONFIG"  --role-arn arn:aws:iam::033374200449:role/eksctl-operations-cluster-admin
+	if [ -n "$AWS_ROLE" ]
+	then
+		aws eks update-kubeconfig --name "$KUBE_CLUSTER" --kubeconfig "$KUBECONFIG" --role-arn "$AWS_ROLE"
+	else
+		aws eks update-kubeconfig --name "$KUBE_CLUSTER" --kubeconfig "$KUBECONFIG"
+	fi
 else
 	echo "$KUBE_CONFIG_DATA" | base64 -d >"$KUBECONFIG"
 fi
 
-#kubectl config view
-#kubectl config get-contexts                          # display list of contexts 
-#kubectl config current-context                       # display the current-context
 exec kubectl "$@"
