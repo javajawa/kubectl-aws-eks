b91ff5fb7efb5779bc2b6a9c4edac4e711897018
Ben Harcourt <ben.harcourt@myvouchercodes.co.uk>
2020-10-19 13:35:57 +0000

    temp

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,7 @@ fi
 
 cat /tmp/config
 
+aws --region eu-west-1 eks get-token --cluster-name operations-cluster
 kubectl config view
 kubectl config get-contexts                          # display list of contexts 
 kubectl config current-context                       # display the current-context
